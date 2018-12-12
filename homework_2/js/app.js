4c42b7a04856417538fa3effd83a2ed8e2110faf
Jacob Mink <jacobmink@gmail.com>
2018-12-12 11:46:55 -0700

    Commit 3 - Excited Kittens answered

--- homework_2/js/app.js
+++ homework_2/js/app.js
@@ -7,3 +7,17 @@ for (let i = 1; i <= 20; i++) {
 for (let i = 0; i <= 200; i+=2) {
     console.log(i);
 }
+
+// EXCITED KITTEN
+
+const kitty_array = ['...human...why you taking pictures of me?...',
+                    '...the catnip made me do it...',
+                    '...why does the red dot always get away...'];
+
+for (let i = 0; i < 20; i++) {
+    console.log(`${i} Love me, pet me! HSSSSSS!`);
+    if (i % 2 == 0) {
+        let rando = Math.floor(Math.random() * 3);
+        console.log(kitty_array[rando]);
+    }
+}
