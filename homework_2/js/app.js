2b97e777b85454f2ed1869708f159676974b8425
Jacob Mink <jacobmink@gmail.com>
2018-12-12 11:58:41 -0700

    Fizz Buzz answered

--- homework_2/js/app.js
+++ homework_2/js/app.js
@@ -21,3 +21,20 @@ for (let i = 0; i < 20; i++) {
         console.log(kitty_array[rando]);
     }
 }
+
+// FIZZBUZZ
+for (let i = 1; i < 101; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i + 'fizzbuzz');
+    }
+    else if (i % 3 == 0) {
+        console.log(i + 'fizz');
+    }
+    else if (i % 5 == 0) {
+        console.log(i + 'buzz');
+    }
+    else {
+        console.log(i);
+    }
+    
+}
